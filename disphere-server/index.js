101ab895ec02b0e4b1efd7187cda77b3683bdce6
Jan-Thomas Riemenschneider <dajanto.1337@hotmail.de>
2020-09-30 15:01:35 +0200

    localhost:4000/graphql geht

--- disphere-server/index.js
+++ disphere-server/index.js
@@ -4,15 +4,9 @@ const schema = require('./schema');
 
 const app = express();
 
-//connect to mlab DB
-//mongoose.connect('mongo "mongodb+srv://cluster0.qzs8d.mongodb.net/Cluster0" --username admin')
-
 app.use('/graphql', graphqlHTTP({
-
-    schema,
-
-    graphql: true,
-
+  schema,
+  graphiql: true,
 }));
 
 app.listen(4000, () => {
